29b260b69c2443e3f7e150ac8168305c1ed35a18
alexisaranda1 <98905408+alexisaranda1@users.noreply.github.com>
2023-11-29 05:23:30 -0300

    Funcionalidad para agregar a lista de espera

--- TpSysacad/FrmGestionListaEspera.cs
+++ TpSysacad/FrmGestionListaEspera.cs
@@ -13,13 +13,13 @@ namespace Formularios
         private CursoPresentador _cursoPresentador;
         private bool esListaDeEstudiantes;
 
+        private Usuario _usuario;
 
-
-        public FrmGestionListaEspera()
+        public FrmGestionListaEspera(Usuario usuario)
         {
             InitializeComponent();
 
-
+            _usuario = usuario;
             _cursoPresentador = new CursoPresentador(this);
             CargarListaCursos();
             btnAgregarEstudiante.Visible = false;
@@ -75,6 +75,7 @@ namespace Formularios
         }
         public void MostrarListaEspera(List<Estudiante> estudiantes)
         {
+            label.Text = "Lista de Espera";
             if (estudiantes != null && estudiantes.Count > 0)
             {
                 dtgvListaEspera.Rows.Clear();
@@ -90,6 +91,7 @@ namespace Formularios
         }
         public void MostrarListaEstudiante(List<Estudiante> estudiantes)
         {
+            label.Text = "Estudiantes disponibles";
             if (estudiantes != null)
             {
                 dtgvListaEspera.Rows.Clear();
@@ -106,6 +108,7 @@ namespace Formularios
 
         public void MostrarCurso(List<Curso> cursos)
         {
+            label.Text = "Cursos no disponibles";
             if (cursos.Count > 0)
             {
                 dtgvListaEspera.Rows.Clear();
@@ -140,13 +143,33 @@ namespace Formularios
             btnEliminarEstudiante.Visible = false;
             btnVerlista.Visible = false;
             btnAgregar.Visible = true;
-            OnListaEstudiantePedida?.Invoke();            
+            OnListaEstudiantePedida?.Invoke();
         }
 
         private void btnEliminarEstudiante_Click_1(object sender, EventArgs e)
         {
+            if (dtgvListaEspera.SelectedRows.Count > 0)
+            {
+                string valorPrimeraColumna = dtgvListaEspera.SelectedRows[0].Cells[0].Value.ToString();
 
+                if (esListaDeEstudiantes)
+                {
+                    _cursoPresentador.LegajoEstudiante = valorPrimeraColumna;
+                }
+                else
+                {
+                    _cursoPresentador.CodigoCurso = valorPrimeraColumna;
+                }
+                int identificador = Convert.ToInt32(valorPrimeraColumna);
+                OnEliminarEstudianteListaEspera?.Invoke(_cursoPresentador.CodigoCurso, identificador);
+                OnListaEsperaPedida?.Invoke();
+            }
+            else
+            {
+                MostrarMensaje("Seleccione una fila para eliminar.");
+            }
         }
+
 
         private void dtgvListaEspera_CellClick(object sender, DataGridViewCellEventArgs e)
         {
@@ -155,7 +178,7 @@ namespace Formularios
                 DataGridViewRow filaSeleccionada = dtgvListaEspera.Rows[e.RowIndex];
 
                 if (filaSeleccionada != null)
-                {                  
+                {
                     DataGridViewCell primeraCelda = filaSeleccionada.Cells[0];
 
                     if (primeraCelda != null && primeraCelda.Value != null)
@@ -164,11 +187,11 @@ namespace Formularios
 
                         if (esListaDeEstudiantes)
                         {
-                            _cursoPresentador.LegajoEstudiante = valorPrimeraColumna;                                                        
+                            _cursoPresentador.LegajoEstudiante = valorPrimeraColumna;
                         }
                         else
                         {
-                            _cursoPresentador.CodigoCurso = valorPrimeraColumna;                                                     
+                            _cursoPresentador.CodigoCurso = valorPrimeraColumna;
                         }
                     }
                 }
@@ -181,8 +204,9 @@ namespace Formularios
             {
                 int legajo = Convert.ToInt32(_cursoPresentador.LegajoEstudiante);
 
-                OnAgregarEstudianteListaEspera?.Invoke(_cursoPresentador.CodigoCurso, legajo);               
+                OnAgregarEstudianteListaEspera?.Invoke(_cursoPresentador.CodigoCurso, legajo);
                 OnListaEsperaPedida?.Invoke();
+
             }
             else
             {
@@ -190,5 +214,16 @@ namespace Formularios
             }
         }
 
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            FormPanelUsuario formPrincipal = new FormPanelUsuario(_usuario);
+            formPrincipal.Show();
+
+        }
+
+
     }
+
+
 }
