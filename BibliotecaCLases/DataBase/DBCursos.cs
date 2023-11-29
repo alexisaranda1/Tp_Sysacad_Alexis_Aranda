2f76716228f801d37a8949d37e60ecaf7d6a4863
Matias Cantero <matuk79@gmail.com>
2023-11-29 00:16:36 -0300

    mejoras en SQLServer

---
+++ BibliotecaCLases/DataBase/DBCursos.cs
@@ -0,0 +1,153 @@
+﻿using BibliotecaCLases.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaCLases.DataBase
+{
+    public class DBCursos:SQLServer
+    {
+        public DBCursos() 
+        {
+
+        }
+        public void Guardar(Curso curso)
+        {
+            try
+            {
+                _conexion.Open();
+                var query = $"INSERT INTO Cursos (Codigo, Nombre, Descripcion, CupoMaximo, CuposDisponibles,Activo, Dia,Horario,Aula) VALUES (@Codigo, @Nombre," +
+                    " @Descripcion, @CupoMaximo, @CuposDisponibles,@Activo,@Dia,@Horario,@Aula)";
+                _comando.CommandText = query;
+
+                //Ajuste de parámetros con valores reales
+                _comando.Parameters.AddWithValue("@Codigo", curso.Codigo);
+                _comando.Parameters.AddWithValue("@Nombre", curso.Nombre);
+                _comando.Parameters.AddWithValue("@Descripcion", curso.Descripcion);
+                _comando.Parameters.AddWithValue("@CupoMaximo", curso.CupoMaximo);
+                _comando.Parameters.AddWithValue("@CuposDisponibles", curso.CuposDisponibles);
+                _comando.Parameters.AddWithValue("@Activo", curso.Activo);
+                _comando.Parameters.AddWithValue("@Dia", curso.Dia);
+                _comando.Parameters.AddWithValue("@Horario", curso.Horario);
+                _comando.Parameters.AddWithValue("@Aula", curso.Aula);
+
+                //comando.Parameters.Clear();
+
+                var filasAfectadas = _comando.ExecuteNonQuery();
+
+                Console.WriteLine("Filas afectadas " + filasAfectadas);
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                _conexion.Close();
+            }
+        }
+        public bool VerificaCodigo(string codigo)
+        {
+            bool existe = false;
+
+            try
+            {
+                _conexion.Open();
+
+                var query = "SELECT COUNT(*) FROM Cursos WHERE Codigo = @Codigo";
+                _comando.CommandText = query;
+
+                _comando.Parameters.AddWithValue("@Codigo", codigo);
+
+                int count = Convert.ToInt32(_comando.ExecuteScalar());
+
+                existe = count > 0;
+            }
+            catch (Exception ex)
+            {
+                // Manejo de excepciones
+            }
+            finally
+            {
+                _conexion.Close();
+            }
+
+            return existe;
+        }
+        public void ModificarCurso(string nombre, string descripcion, string cupoMaximo, string codigo)
+        {
+            try
+            {
+                _conexion.Open();
+
+                // Consulta SQL para actualizar un curso
+                var query = $"UPDATE Cursos SET Codigo = @Codigo, Nombre = @Nombre, CupoMaximo = @CupoMaximo, Descripcion = @Descripcion";
+                _comando.CommandText = query;
+
+                // Ajuste de parámetros con los nuevos valores
+                _comando.Parameters.AddWithValue("@Codigo", codigo);
+                _comando.Parameters.AddWithValue("@Nombre", nombre);
+                _comando.Parameters.AddWithValue("@CupoMaximo", cupoMaximo);
+                _comando.Parameters.AddWithValue("@Descripcion", descripcion);
+
+                // Ejecución de la consulta
+                int filasAfectadas = _comando.ExecuteNonQuery();
+
+                if (filasAfectadas > 0)
+                {
+                    Console.WriteLine("Registro actualizado exitosamente");
+                }
+                else
+                {
+                    Console.WriteLine("No se encontró el curso con el código proporcionado");
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                _conexion.Close();
+            }
+        }
+
+        public void BorrarCurso(int codigoCurso)
+        {
+            try
+            {
+                _conexion.Open();
+
+                // Consulta SQL para borrar un curso
+                var query = "DELETE FROM Cursos WHERE CodigoCurso = @CodigoCurso";
+                _comando.CommandText = query;
+
+                // Ajuste de parámetros con el código del curso a borrar
+                _comando.Parameters.AddWithValue("@CodigoCurso", codigoCurso);
+
+                // Ejecución de la consulta
+                int filasAfectadas = _comando.ExecuteNonQuery();
+
+                if (filasAfectadas > 0)
+                {
+                    Console.WriteLine("Registro borrado exitosamente");
+                }
+                else
+                {
+                    Console.WriteLine("No se encontró el curso con el código proporcionado");
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                _conexion.Close();
+            }
+        }
+
+    }
+}
---
+++ BibliotecaCLases/DataBase/DBEstudiantes.cs
@@ -0,0 +1,116 @@
+﻿using BibliotecaCLases.Interfaces;
+using BibliotecaCLases.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaCLases.DataBase
+{
+    public class DBEstudiantes<T>:SQLServer where T : Estudiante
+    {
+        public DBEstudiantes()
+        {
+
+        }
+
+        public void Guardar(Estudiante estudiante)
+        {
+            try
+            {
+                _conexion.Open();
+                var query = $"INSERT INTO Estudiante (Legajo, Nombre, Apellido, Correo, DNI,Clave, TipoUsuario,DebeCambiar,Direccion,Telefono,CursosInscriptos,EstadoDePago) VALUES (@Legajo, @Nombre," +
+                    " @Apellido, @Correo, @Dni,@Clave,@TipoUsuario,@DebeCambiar,@Direccion,@Telefono,@CursosInscriptos,@EstadoDePago)";
+                _comando.CommandText = query;
+
+                // Ajuste de parámetros con valores reales
+                _comando.Parameters.AddWithValue("@Legajo", estudiante.Legajo);
+                _comando.Parameters.AddWithValue("@Nombre", estudiante.Nombre);
+                _comando.Parameters.AddWithValue("@Apellido", estudiante.Apellido);
+                _comando.Parameters.AddWithValue("@Correo", estudiante.Correo);
+                _comando.Parameters.AddWithValue("@Dni", estudiante.Dni);
+                _comando.Parameters.AddWithValue("@Clave", estudiante.Clave);
+                _comando.Parameters.AddWithValue("@TipoUsuario", estudiante.TipoUsuario);
+                _comando.Parameters.AddWithValue("@DebeCambiar", estudiante.Debecambiar);
+                _comando.Parameters.AddWithValue("@Direccion", estudiante.Direccion);
+                _comando.Parameters.AddWithValue("@Telefono", estudiante.Telefono);
+                _comando.Parameters.AddWithValue("@CursosInscriptos", estudiante.CursosInscriptos);
+                _comando.Parameters.AddWithValue("@EstadoPago", estudiante.EstadoDePago);
+
+                //comando.Parameters.Clear();
+
+                var filasAfectadas = _comando.ExecuteNonQuery();
+
+                Console.WriteLine("Filas afectadas " + filasAfectadas);
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                _conexion.Close();
+            }
+        }
+
+        public bool VerificaDni(string dni)
+        {
+            bool existe = false;
+
+            try
+            {
+                _conexion.Open();
+
+                var query = "SELECT COUNT(*) FROM Estudiante WHERE DNI = @Dni";
+                _comando.CommandText = query;
+
+                _comando.Parameters.AddWithValue("@DNI", dni);
+
+                int count = Convert.ToInt32(_comando.ExecuteScalar());
+
+                existe = count > 0;
+            }
+            catch (Exception ex)
+            {
+                // Manejo de excepciones
+            }
+            finally
+            {
+                _conexion.Close();
+            }
+
+            return existe;
+        }
+
+        public bool VerificaMail(string mail)
+        {
+            bool existe = false;
+
+            try
+            {
+                _conexion.Open();
+
+                var query = "SELECT COUNT(*) FROM Estudiante WHERE Correo = @Mail";
+                _comando.CommandText = query;
+
+                _comando.Parameters.AddWithValue("@Mail", mail);
+
+                int count = Convert.ToInt32(_comando.ExecuteScalar());
+
+                existe = count > 0;
+            }
+            catch (Exception ex)
+            {
+                // Manejo de excepciones
+            }
+            finally
+            {
+                _conexion.Close();
+            }
+
+            return existe;
+        }
+    }
+}
